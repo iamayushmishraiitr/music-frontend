0440ab09ba087057569a2b0c79bc0c68e5e4128e
iamayushmishtaiitr <iamayushmishra1510@gmail.com>
2025-07-14 16:24:51 +0530

    changed baseUrl

--- src/socketContext/WebSocketConnection.ts
+++ src/socketContext/WebSocketConnection.ts
@@ -1,11 +1,11 @@
 import { useEffect, useRef, useState } from "react";
-
+import { baseUrl } from "../Api/reqHandler";
 export const useWebSocketConnection = () => {
   const socketRef = useRef<WebSocket | null>(null);
   const [socket, setSocket] = useState<WebSocket | null>(null);
 
   useEffect(() => {
-    const ws = new WebSocket("ws://localhost:3000");
+    const ws = new WebSocket(baseUrl);
     socketRef.current = ws;
 
     ws.onopen = () => {
